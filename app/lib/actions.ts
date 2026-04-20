55490a7ce0155b995c62b3db23f7afde94611ddb
bnsn <bdiep45@gmail.com>
2026-04-20 15:57:14 -0700

    testing

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,7 +59,7 @@ export async function updateInvoice(id: string, formData: FormData) {
     `;
   } catch (error) {
     console.error(error);
-    return { message: "Database Error: Failed to Update Invoice" };
+    // return { message: "Database Error: Failed to Update Invoice" };
   }
 
   revalidatePath("/dashboard/invoices");
